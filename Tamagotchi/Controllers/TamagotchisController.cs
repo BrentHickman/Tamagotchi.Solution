f3ca19eec80ebf02bb872f914c2117027483048a
BrentHickman <BrentHickman@ymail.com>
2023-03-01 14:14:30 -0800

    Feed and rest work

--- Tamagotchi/Controllers/TamagotchisController.cs
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -35,6 +35,22 @@ namespace Tamagotchi.Controllers
       Pet.Eat(passInId);
       return View();
     }
+
+    [HttpPost("/tamagotchis/play")]
+    public ActionResult Play(int passInId)
+    {
+      string petName = Pet.Find(passInId).Name;
+      Pet.Play(passInId);
+      return View();
+    }
+
+    [HttpPost("/tamagotchis/rest")]
+    public ActionResult Rest(int passInId)
+    {
+      string petName = Pet.Find(passInId).Name;
+      Pet.Rest(passInId);
+      return View();
+    }
     
     [HttpPost("/tamagotchis/delete")]
     public ActionResult DeleteAll()
