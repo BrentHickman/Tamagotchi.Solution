71e7511a41365875603e95208f7a1ab29760482d
BrentHickman <BrentHickman@ymail.com>
2023-03-01 12:37:18 -0800

    Tamagotchi controller set up
    Co-authored-by: Charles <charlesbcookman@gmail.com>

--- Tamagotchi/Controllers/TamagotchisController.cs
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Tamagotchi.Models;
 using System.Collections.Generic;
-using System;
+
 
 namespace Tamagotchi.Controllers
 {
--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
 
-namespace Pet.Models
+namespace Tamagotchi.Models
 {
   public class Pet
   {
     public string Name { get; set; }
-    public int Id { get; }
-    public int Food;
-    public int Happy;
-    public int Energy;
+    public int Id { get; set; }
+    public int Food { get; set; }
+    public int Happy{ get; set; }
+    public int Energy { get; set; }
     private static List<Pet> _instances = new List<Pet> { };
 
     public Pet(string name)
@@ -21,20 +21,20 @@ namespace Pet.Models
       Energy = 5;
     }
 
-    public static void Eat()
+    public void Eat()
     {
       Food = Food + 4;
       Happy = Happy + 1;
       Energy = Energy -1;
     }
 
-    public static void Play()
+    public void Play()
     {
       Food = Food - 1;
       Happy = Happy + 4;
       Energy = Energy - 2;
     }
-    public static void Rest()
+    public void Rest()
     {
       Food = Food - 2;
       Happy = Happy + 1;
